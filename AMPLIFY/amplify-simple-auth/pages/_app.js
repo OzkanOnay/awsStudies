19b28e09ce1d20ffadda7066502457cb12a9b740
Özkan Onay <ozkanonay@gmail.com>
2021-12-19 17:37:12 +0300

    amplify authentication

--- AMPLIFY/amplify-simple-auth/pages/_app.js
+++ AMPLIFY/amplify-simple-auth/pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
 
+import Amplify, { AuthModeStrategyType } from 'aws-amplify'
+import awsconfig from '../src/aws-exports'
+
+Amplify.configure({
+  ...awsconfig,
+  DataStore: {
+    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH
+  }
+})
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
