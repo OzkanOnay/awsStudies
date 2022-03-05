1b2873ce1943a3202ff9dcf06408b545b99c9f7f
ozkanonay <ozkanonay@gmail.com>
2022-03-05 13:44:40 +0300

    lambda manning liveproject museum

---
+++ lambda/book-museum/index.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (evt) => {    
+    const url = "https://ktvs5s43xd.execute-api.eu-central-1.amazonaws.com/Prod/museum";
+
+    let response = await fetch(url,{
+            method:'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(evt)
+        }
+    );
+
+    let json = await response.json();
+
+    return json;
+}
